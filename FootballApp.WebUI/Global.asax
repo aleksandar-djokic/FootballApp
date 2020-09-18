7dd61e4b4f43c9d535ac4afae0016b1b5aa2134f
Aleksandar <aleksandar3717@its.edu.rs>
2020-09-18 11:37:50 +0200

    Add project files.

---
+++ FootballApp.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FootballApp.WebUI.MvcApplication" Language="C#" %>
---
+++ FootballApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FootballApp.WebUI.Startup))]
+namespace FootballApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
