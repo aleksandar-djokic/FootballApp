00e9be6873e7b28cda0eddecc5547b569b944762
Aleksandar <aleksandar3717@its.edu.rs>
2020-09-18 12:00:33 +0200

    EnableMigrations
    Enabling Migrations for Ef Codefirst approach

---
+++ FootballApp.Domain/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace FootballApp.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FootballApp.Domain.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "FootballApp.Domain.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(FootballApp.Domain.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
