4e2bc0b3193ec918d563750153260a1640def345
Aleksandar <aleksandar3717@its.edu.rs>
2020-09-23 13:57:50 +0200

    Setting up Autofac DI
    Adding nuget packages for Autofac.
    Setting up autofac.

--- FootballApp.WebUI/Global.asax.cs
+++ FootballApp.WebUI/Global.asax.cs
@@ -5,13 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace FootballApp.WebUI
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        {   //Autofac DI setup
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //RegisterDependency
+            //builder.RegisterType<Product2Repository>().As<IProductRepository>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
