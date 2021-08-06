f2b67b8d926bc980315f498e83786cbf10cb9c4e
aleksandar-djokic <aleksandar.djokic.contact@gmail.com>
2021-08-06 12:30:41 +0200

    Unit Test
    Unit test using Moq repository

---
+++ FootballAppTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FootballAppTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FootballAppTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1250882e-5bbf-4965-8081-cf39bd7238a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FootballAppTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+using FootballApp.Domain.Abstract;
+using FootballApp.Domain.Models;
+using System.Collections.Generic;
+using FootballApp.WebUI.Controllers;
+using FootballApp.WebUI.Models;
+using System.Web.Mvc;
+
+namespace FootballAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void Team_Can_Change_Name()
+        {
+            Mock<ITeamRepository> mock = new Mock<ITeamRepository>();
+            var imeTima = "Proba";
+            TeamController tc = new TeamController(mock.Object);
+
+            var timZaIzmenu = new TeamViewModel()
+            {
+                Id = 1,
+                TeamName = "ImeTima",
+                Description = "OpisTima",
+                Picture = null
+                
+
+            };
+            mock.Setup(x => x.isNameTaken(It.IsAny<string>(),It.IsAny<int>())).Returns(false);
+            mock.Setup(x => x.Edit(timZaIzmenu.Id, timZaIzmenu.TeamName, timZaIzmenu.Description, null)).Callback((() => { timZaIzmenu.TeamName = imeTima; })) ;
+            tc.Edit(timZaIzmenu);
+           
+            Assert.AreEqual(imeTima,timZaIzmenu.TeamName);
+        }
+    }
+}
